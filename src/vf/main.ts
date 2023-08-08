a1555e9ee344a6af5d800f364d3882e028e91024
Zecyel <zecyel@163.com>
2023-08-08 23:25:00 +0800

    Fix: fix drag bugs

--- src/vf/main.ts
+++ src/vf/main.ts
@@ -34,7 +34,7 @@ function add_new_div(el: HTMLDivElement) {
     let div = document.createElement('div') as HTMLDivElement
     div.style.width = '120px'
     div.style.height = '80px'
-    div.style.position = 'relative'
+    div.style.position = 'absolute'
     div.style.background = '#3366ff'
     div.style.top = '0px'
     div.style.left = '0px'
@@ -57,8 +57,8 @@ function mousedownHandler(this: HTMLDivElement, ev: MouseEvent){
     item_on_hand = true
     current_object = this
 
-    console.log('offset', offsetX, offsetY)
     console.log('mouse', mouseX, mouseY)
+    console.log('offset', offsetX, offsetY)
 }
 
 function mousemoveHandler(ev: MouseEvent){
@@ -66,18 +66,24 @@ function mousemoveHandler(ev: MouseEvent){
     if(! item_on_hand){
         return;
     }
+
+    console.log(ev)
     
     let mouseX_new = ev.clientX
     let mouseY_new = ev.clientY
     
+    console.log('mouse new', mouseX_new, mouseY_new)
+
     let offsetX_new = offsetX + mouseX_new - mouseX
-    let offsetY_new = offsetY + mouseY_new - mouseY - 80
+    let offsetY_new = offsetY + mouseY_new - mouseY
+
+    console.log('offset new', offsetX_new, offsetY_new)
     
     // if (offsetX_new < 100) offsetX_new = 0
     // if (offsetY_new < 100) offsetY_new = 0
 
-    current_object.style.left = offsetX_new + 'px';
-    current_object.style.top = offsetY_new + 'px';
+    current_object.style.left = offsetX_new + 'px'
+    current_object.style.top = offsetY_new + 'px'
 }
 
 export default inject
