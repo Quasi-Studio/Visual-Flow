ee17d4179754fa7403c0ee53c0a058bb23470a2b
Zecyel <zecyel@163.com>
2023-08-11 00:13:39 +0800

    Upd: 成功引入了mdui，写了一个简单的分栏页面

--- src/vf/toolkit/toolkit.ts
+++ src/vf/toolkit/toolkit.ts
@@ -5,7 +5,21 @@ class ToolKit extends ElementBase<HTMLDivElement> {
     constructor () {
         super(document.createElement('div'))
     }
+    
+    initialize() {
+        let lst = document.createElement('ul')
+        lst.classList.add('mdui-list')
+        this.register(lst)
+        for (let i of item) {
+            let _el = document.createElement('li')
+            _el.textContent = i
+            _el.classList.add('mdui-list-item')
+            lst.appendChild(_el)
+        }
+    }
 }
+
+const item = ["123", "456", "567"]
 
 export {
     ToolKit
--- src/vf/workspace.ts
+++ src/vf/workspace.ts
@@ -14,7 +14,19 @@ class Workspace extends ElementBase<HTMLDivElement> {
         
         this.flowgraph = new FlowGraph()
         this.register(this.flowgraph)
+
+        this.layout()
+
+        this.toolkit.initialize()
+        mdui.mutation()
     }
+
+    layout() {
+        this.el.classList.add('mdui-container-fluid')
+        this.toolkit.el.classList.add('mdui-col-xs-3')
+        this.flowgraph.el.classList.add('mdui-col-xs-9')
+    }
+
 }
 
 export {
